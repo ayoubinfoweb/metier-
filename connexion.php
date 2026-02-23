97f1108e3b52a5da551f700fab2d6db1ba6e0820
ayoub <ayoub@gmail.com>
2026-02-23 13:52:55 +0000

    ajouter le fichier de connexion

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+/*connexion.php*/
+session_start();
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "metier";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+/*utf8*/
+mysqli_set_charset($conn, "utf8");
+
+?>
+
